a2efa59cc5be585873c67f1761d67da4ab216678
Jeremy Cook <jeremy.cook@cloudacademy.com>
2024-11-04 17:21:17 +1300

    updates

--- main.tf
+++ main.tf
@@ -13,29 +13,96 @@ resource "aws_s3_bucket" "website" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_website_configuration" "this" {
-  bucket = aws_s3_bucket.website.bucket
-  index_document {
-    suffix = var.index_document
-  }
-  error_document {
-    key = var.error_document
-  }
-}
-
-resource "aws_s3_bucket_policy" "allow_public_read" {
+resource "aws_s3_bucket_ownership_controls" "ownership" {
   bucket = aws_s3_bucket.website.id
-  policy = data.aws_iam_policy_document.allow_public_read.json
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
-data "aws_iam_policy_document" "allow_public_read" {
-  statement {
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
+resource "aws_s3_bucket_public_access_block" "publiceaccess" {
+  bucket                  = aws_s3_bucket.website.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.website.arn}/*"]
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.ownership,
+    aws_s3_bucket_public_access_block.publiceaccess,
+  ]
+
+  bucket = aws_s3_bucket.website.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.website.id
+  key          = "index.html"
+  source       = "./website/index.html"
+  content_type = "text/html"
+}
+
+# resource "aws_s3_object" "error" {
+#   bucket       = aws_s3_bucket.website.id
+#   key          = "error.html"
+#   source       = "error.html"
+#   content_type = "text/html"
+# }
+
+# resource "aws_s3_object" "assets_folder" {
+#   for_each = fileset("assets", "**/*")
+#   bucket   = aws_s3_bucket.website.id
+#   key      = each.key
+#   source   = "assets/${each.key}"
+# }
+
+# resource "aws_s3_object" "error_folder" {
+#   for_each = fileset("error_files", "**/*")
+#   bucket   = aws_s3_bucket.website.id
+#   key      = each.key
+#   source   = "error_files/${each.key}"
+# }
+
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
   }
+
+  # error_document {
+  #   key = "error.html"
+  # }
+
+  # routing_rule {
+  #   condition {
+  #     key_prefix_equals = "assets/"
+  #   }
+  #   redirect {
+  #     replace_key_prefix_with = "assets/"
+  #   }
+  # }
+}
+
+resource "aws_s3_bucket_policy" "public_read_access" {
+  bucket = aws_s3_bucket.website.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+   "Principal": "*",
+      "Action": [ "s3:GetObject" ],
+      "Resource": [
+        "${aws_s3_bucket.website.arn}",
+        "${aws_s3_bucket.website.arn}/*"
+      ]
+    }
+  ]
+}
+EOF
 }
